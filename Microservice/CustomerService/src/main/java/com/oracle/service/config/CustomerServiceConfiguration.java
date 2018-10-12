261dc93b539cbc086c2239ada934f458770b2807
tkdey <tkdey@TKDEY-IN.idc.oracle.com>
2018-10-12 14:48:20 +0530

    Clean Code

--- Microservice/CustomerService/src/main/java/com/oracle/service/config/CustomerServiceConfiguration.java
+++ Microservice/CustomerService/src/main/java/com/oracle/service/config/CustomerServiceConfiguration.java
@@ -1,21 +1,12 @@
 package com.oracle.service.config;
 
-import java.math.BigDecimal;
-import java.util.List;
-import java.util.Map;
-import java.util.Random;
-import java.util.logging.Logger;
-
 import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 
 @Configuration
