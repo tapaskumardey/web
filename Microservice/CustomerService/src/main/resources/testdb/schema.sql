e25272e2d6b571d65175bd1f69f34fc459801759
tkdey <tkdey@TKDEY-IN.idc.oracle.com>
2018-10-15 09:22:07 +0530

    Removed CustomerService & ProductService

--- Microservice/CustomerService/src/main/resources/testdb/schema.sql
+++
@@ -1,4 +0,0 @@
-drop table T_CUSTOMER if exists;
-
-create table T_CUSTOMER (CUSTOMER_ID bigint identity primary key,
-                        CUSTOMER_NAME varchar(150) not null);
--- Microservice/ProductService/src/main/resources/testdb/data.sql
+++
@@ -1,7 +0,0 @@
-insert into T_PRODUCT (PRODUCT_ID, PRODUCT_NAME) values ('1000', 'Newspapers and magazines');
-insert into T_PRODUCT (PRODUCT_ID, PRODUCT_NAME) values ('1001', 'Most groceries and food items');
-insert into T_PRODUCT (PRODUCT_ID, PRODUCT_NAME) values ('1002', 'Soft drinks and snack foods and candy bars');
-insert into T_PRODUCT (PRODUCT_ID, PRODUCT_NAME) values ('1003', 'Tissues, headache tablets');
-insert into T_PRODUCT (PRODUCT_ID, PRODUCT_NAME) values ('1004', 'Bread and milk, breakfast cereals');
-insert into T_PRODUCT (PRODUCT_ID, PRODUCT_NAME) values ('1005', 'Toothpaste, soap, and shampoo');
-insert into T_PRODUCT (PRODUCT_ID, PRODUCT_NAME) values ('1006', 'Cleaning products, dish-washing powder, detergents');
